febd15270f7eeec21c26b4a49f18ddb20b0c66f9
lukezheng2011 <lukezheng2011@gmail.com>
2025-09-12 08:50:08 -0400

    Create ninetydegreespiral.py

---
+++ ninetydegreespiral.py
@@ -0,0 +1,49 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+t.speed(123)
+
+length=2
+
+def line(length):
+    for i in range(2):
+        t.forward(length)
+        t.right(90)
+
+for i in range(1111):
+    
+    line(length)
+    length+=2
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()
